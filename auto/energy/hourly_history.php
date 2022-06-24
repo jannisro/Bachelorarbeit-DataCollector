95b1f68c54033d26463432d447d8b4b58df9ef8d
Jannis Rohden <kontakt@jannisrohden.de>
2022-06-24 16:33:44 +0200

    Added hourly live data fetch

---
+++ auto/energy/hourly_history.php
@@ -0,0 +1,48 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use DataCollector\DatabaseAdapter;
+use Symfony\Component\Dotenv\Dotenv;
+
+
+// Parse .env file with configuration
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/../../.env');
+
+class HistoryFetch extends DatabaseAdapter
+{
+
+    public function __invoke()
+    {
+        $this->runDbMultiQuery("DELETE FROM `electricity_generation` WHERE `datetime` LIKE '0000-%'");
+        $minDate = $this->runDbQuery(
+            "SELECT `datetime` 
+            FROM `electricity_generation` 
+            ORDER BY `datetime` ASC 
+            LIMIT 1"
+        )[0]['datetime'];
+        $dt = (new \DateTime($minDate))->modify('-1 day');
+        if ($dt) {
+            echo 'Collect data for ' . $dt->format('Y-m-d');
+            $classes = [
+                "ElectricityPrice",
+                "CommercialFlow",
+                "PhysicalFlow",
+                "Generation",
+                "Load",
+                "ForecastedLoad",
+                "NetTransferCapacity"
+            ];
+            foreach ($classes as $class) {
+                $call = "DataCollector\\Energy\\$class";
+                (new $call)(\DateTimeImmutable::createFromMutable($dt));
+                sleep(5);
+            }
+            (new DataCollector\Energy\NetPosition)(\DateTimeImmutable::createFromMutable($dt));
+        }
+    }
+
+}
+
+(new HistoryFetch)();
